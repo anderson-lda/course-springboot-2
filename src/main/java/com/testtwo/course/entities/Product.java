3c1a7578607000efa206523030975e633b4a4c61
anderson-lda <anderson.lima.araujo@hotmail.com>
2021-07-06 11:16:36 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/testtwo/course/entities/Product.java
+++ src/main/java/com/testtwo/course/entities/Product.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -33,6 +36,9 @@ public class Product implements Serializable {
 	inverseJoinColumns = @JoinColumn(name = "category_id")) //já configura para a outra parte também
 	private Set<Category> categories = new HashSet<>(); //conjunto que garante que não haja elementos repetitivos;
 	//ao instanciar, garantimos que a coleção não inicialize nula e sim vazia
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 		
@@ -78,6 +84,15 @@ public class Product implements Serializable {
 	public Set<Category> getCategories() {
 		return categories;
 	} //categories não possui set pois não faz sentido trocarmos elementos dela
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x: items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
 	
 	@Override
 	public int hashCode() {
